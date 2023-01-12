e9077e0334905b19b98518e7204c5c9e3190fad5
shir <shir8806@gmail.com>
2023-01-12 20:57:53 +0200

    fix navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,11 +14,11 @@ export function Navbar () {
     <>
 
     <nav className="navbar fixed-top" style={{backgroundColor: '#E0F6EE'}}>
-          <span className="username" style={{marginLeft: "80px", top: "40%", position: "absolute", fontSize: "1rem"}}>
+          <span className="username" style={{marginLeft: "80px", top: "30%", position: "absolute", fontSize: "1rem"}}>
             <strong>Hello {userName}</strong>
           </span>
           <a className="logout" 
-            style={{marginLeft: "22px"}} 
+            style={{marginLeft: "92px", top: "50%", position: "absolute"}} 
             href="/"
             onClick={()=> {
               window.localStorage.clear();
@@ -29,13 +29,13 @@ export function Navbar () {
         
         <div className="container-fluid d-flex justify-content-between">
           <ModalPop/>
-          <a className="navbar-brand fs-2" style={{marginLeft: "22px"}} href="#"><strong>🆆🤓🆁 🅳 🅻 🅴</strong></a>
+          <a className="navbar-brand fs-2" style={{marginLeft: "22px"}} href="/"><strong>🆆🤓🆁 🅳 🅻 🅴</strong></a>
           <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="offcanvas offcanvas-end" tabIndex={-1} id="offcanvasNavbar">
             <div className="offcanvas-header">
-              <h5 className="offcanvas-title" id="offcanvasNavbarLabel">WOOORDLE</h5>
+              <h5 className="offcanvas-title" id="offcanvasNavbarLabel">🆆🤓🆁 🅳 🅻 🅴</h5>
               <button type="button" className="btn-close" data-bs-dismiss="offcanvas"></button>
             </div>
             <div className="offcanvas-body">
@@ -43,9 +43,9 @@ export function Navbar () {
                 <li className="nav-item">
                   <a className="nav-link active"  href="/">Home</a>
                 </li>
-                <li className="nav-item">
+                {/* <li className="nav-item">
                   <a className="nav-link" href="#">Sign in</a>
-                </li>
+                </li> */}
               </ul>
             </div>
           </div>
